c57dce6265a95c743373f9523e01ad81d15150d5
mallory-erik <malloryerik@gmail.com>
2019-02-24 01:10:32 +0900

    Added first mutation -- Post

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return (
+      <div className="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
+        <CreateLink />
+        <LinkList />
+      </div>
+
+    )
   }
 }
 
