445976dc7066703c810eead2a3cf8667b42b6e2c
mallory-erik <malloryerik@gmail.com>
2019-03-03 16:12:50 +0900

    newLink subscription

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,6 +3,27 @@ import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+	subscription {
+		newLink {
+			id
+			url
+			description
+			createdAt
+			postedBy {
+				id
+				name
+			}
+			votes {
+				id
+				user {
+					id
+				}
+			}
+		}
+	}
+`
+
 export const FEED_QUERY = gql`
 	{
 		feed {
@@ -35,30 +56,53 @@ class LinkList extends Component {
 
 		store.writeQuery({ query: FEED_QUERY, data })
 	}
+
+	_subscribeToNewLinks = subscribeToMore => {
+		subscribeToMore({
+			document: NEW_LINKS_SUBSCRIPTION,
+			updateQuery: (prev, { subscriptionData }) => {
+				if (!subscriptionData.data) return prev
+				const newLink = subscriptionData.data.newLink
+				const exists = prev.feed.links.find(({ id }) => id === newLink.id)
+				if (exists) return prev
+
+				return Object.assign({}, prev, {
+					feed: {
+						links: [newLink, ...prev.feed.links],
+						count: prev.feed.links.length + 1,
+						__typename: prev.feed.__typename
+					}
+				})
+			}
+		})
+	}
+
 	render() {
-		return(
+		return (
 			<Query query={FEED_QUERY}>
-			{({ loading, error, data }) => {
-				if (loading) return <div>Fetching</div>
-				if (error) return <div>Error</div>
+				{({ loading, error, data, subscribeToMore }) => {
+					if (loading) return <div>Fetching</div>
+					if (error) return <div>Error</div>
 
-				const linksToRender = data.feed.links
+					this._subscribeToNewLinks(subscribeToMore)
 
-				return (
-					<div>
-						{linksToRender.map((link, index) => 
-							<Link 
-								key={link.id} 
-								link={link} 
-								index={index}
-								updateStoreAfterVote={this._updateCacheAfterVote}
-							/>
-						)}
-					</div>
-				)
-			}}				
+					const linksToRender = data.feed.links
+
+					return (
+						<div>
+							{linksToRender.map((link, index) =>
+								<Link
+									key={link.id}
+									link={link}
+									index={index}
+									updateStoreAfterVote={this._updateCacheAfterVote}
+								/>
+							)}
+						</div>
+					)
+				}}
 			</Query>
-		) 
+		)
 	}
 }
 
